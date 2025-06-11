c397952fe827b4017e210e618e70112dc82b6208
ihateusernamesfuckthis <pepe0003@kea.stud.dk>
2025-06-11 07:32:48 +0200

    første commit

---
+++ src/main/java/com/example/teknologicicd/TeknologiCicdApplication.java
@@ -0,0 +1,20 @@
+package com.example.teknologicicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TeknologiCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeknologiCicdApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(){
+		return "der er hul igennem";
+	}
+}
---
+++ src/test/java/com/example/teknologicicd/TeknologiCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teknologicicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
